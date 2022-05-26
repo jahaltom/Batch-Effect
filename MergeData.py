3d84a53fc400f2a41e4517d92747b6da27d0e9bf
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2022-05-26 15:55:39 -0500

    Update MergeData.py

--- MergeData.py
+++ MergeData.py
@@ -2,18 +2,17 @@ import pandas as pd
 
 
 
-###########Join Nasal and Autopsy quant data
 nasal_counts = pd.read_csv("results_Count_gene.nasal.tsv",sep='\t')
 nasal_TPM = pd.read_csv("results_TPM_gene.nasal.tsv",sep='\t')
 autopsy_counts = pd.read_csv("results_Count_gene.autopsy.tsv",sep='\t')
 autopsy_TPM = pd.read_csv("results_TPM_gene.autopsy.tsv",sep='\t')
 metadata=pd.read_csv("Metadata.txt",sep='\t')
 
-#Get autopsy columns only
+#Get autopsy columns names only
 autopsyColsCounts = autopsy_counts.columns.difference(nasal_counts.columns)
 autopsyColsTPM = autopsy_TPM.columns.difference(nasal_TPM.columns)
 
-#Merge autopsy with nasal
+#Merge nasal with autopsy
 NasalAutopsyCount=pd.merge(nasal_counts, autopsy_counts[autopsyColsCounts], left_index=True, right_index=True, how='outer')
 NasalAutopsyTPM=pd.merge(nasal_TPM, autopsy_TPM[autopsyColsTPM], left_index=True, right_index=True, how='outer')
 

396948153139676c29c0a81583cc72c6584755a9
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2022-06-03 16:38:06 -0500

    Create ComBat-Seq.r

---
+++ ComBat-Seq.r
@@ -0,0 +1,22 @@
+library(dplyr)
+
+#Read in count data.
+countData = read.table("results_Count_gene.filtered.tsv",header=TRUE,sep = '\t')
+#X an Y gene names can be the same. This makes them unique. Row is set to this unique value. 
+rownames(countData)= paste(countData$Gene_name,countData$Gene_stable_ID, countData$chr,sep="$")
+
+#Select only columns with count data. 
+countData=select(countData,contains(c("COVSUBJ","batch")))
+#Round expression data.
+countData=round(countData,0) 
+
+
+##ComBat-Seq
+metadata = read.table("Metadata.tsv",header=TRUE,sep = '\t')
+group=metadata$Tissue
+batch=metadata$Study
+
+adjusted_counts = sva::ComBat_seq(countData, batch=batch, group=group, full_mod=FALSE)
+write.table(adjusted_counts,"CombatSeq.Counts.tsv" ,sep = '\t',quote = FALSE)
+
+

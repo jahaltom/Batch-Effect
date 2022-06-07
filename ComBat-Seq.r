e630b05d18bd45b99a5e779f7795ed3ef5ce5a1e
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2022-06-07 13:23:09 -0500

    Update ComBat-Seq.r

--- ComBat-Seq.r
+++ ComBat-Seq.r
@@ -1,7 +1,7 @@
 library(dplyr)
 
 #Read in count data.
-countData = read.table("results_Count_gene.filtered.tsv",header=TRUE,sep = '\t')
+countData = read.table("results_Count_gene.filtered.tsv",header=TRUE,sep = '\t',quote="")
 #X an Y gene names can be the same. This makes them unique. Row is set to this unique value. 
 rownames(countData)= paste(countData$Gene_name,countData$Gene_stable_ID, countData$chr,sep="$")
 

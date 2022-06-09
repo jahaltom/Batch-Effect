1763345072177baf143e4e069aebbfbd1155d80d
Jeff Haltom <52675783+jahaltom@users.noreply.github.com>
2022-06-09 17:04:59 -0500

    Update Plot.r

--- Plot.r
+++ Plot.r
@@ -1,22 +1,34 @@
 ###############
 library(tibble)
 library("ggplot2")
-#Read in count and metadata. 
-Data = read.table("DESeq_TPM.tsv", header=TRUE,sep='\t')
-Design=read.table("Metadata.tsv", header=TRUE,sep='\t')
+library(dplyr)
+
+#Read in counts and metadata. 
+Data = read.table("results_Count_gene.filtered.tsv", header=TRUE,sep='\t',quote="")
+metadata=read.table("Metadata.txt", header=TRUE,sep='\t')
+
+
+#X an Y gene names can be the same. This makes them unique. Row is set to this unique value. 
+rownames(Data)= paste(Data$Gene_name,Data$Gene_stable_ID,Data$chr,sep="$")
+
+#Select only columns with count data. 
+Data=select(Data,contains(c("COVSUBJ","batch")))
+
 #Remove zero variance columns
 Data=t(Data)[ , which(apply(t(Data), 2, var) !=0)]
 #PCA
 data.pca = prcomp(Data,center=TRUE,scale=TRUE)
 
-
 #Make SampleID column
-data.pca <- tibble::rownames_to_column(as.data.frame(data.pca$x), "SampleID")
+data.pca = tibble::rownames_to_column(as.data.frame(data.pca$x), "SampleID")
+#Merge with metadata
+data.pca=merge(metadata,data.pca,all.by="SampleID",all.x = TRUE,all.y=TRUE)
 
-data.pca=merge(Design,data.pca,all.by="SampleID",all.x = TRUE,all.y=TRUE)
-
-
-ggplot(data.pca, aes(x = PC1, y = PC2, color = Study)) +
+#Plot
+ggplot(data.pca, aes(x = PC1, y = PC2, color = Tissue)) +
   geom_point(size =2) +
   #coord_fixed() +
-  ggtitle("Autopsy Nasal ComBat-Seq")
+  ggtitle("CombatSeq.Counts")
+png("CombatSeq.Counts.png")
+print(p)
+dev.off()
